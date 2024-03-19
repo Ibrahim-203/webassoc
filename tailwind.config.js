4bdd58eac0e78d6b8d983415073112dae9127446
Ibrahim-203 <anizwamiibra@gmail.com>
2024-03-20 01:33:46 +0300

    initialisation de tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./pages/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mainFont: ['novaMono'],
+        secondaryFont : ['lato']
+      },
+  },
+    
+  },
+  plugins: [],
+}
+
