21cf0d25bd2065c0c47162488c7db5bdd6e8bc2f
Ibrahim-203 <anizwamiibra@gmail.com>
2024-03-28 08:58:13 +0300

    Page accueil react : section concepts

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const navItems = [
-    { id: 1, text: "Accueil" },
-    { id: 2, text: "Activités" },
-    { id: 3, text: "Evenement" },
-    { id: 4, text: "A propos" },
-    { id: 5, text: "Contact" },
+    { id: 1, text: "Accueil",link:"/" },
+    { id: 2, text: "Activités",link:"/activity" },
+    { id: 3, text: "Evenement",link:"/event" },
+    { id: 4, text: "A propos",link:"/about" },
+    { id: 5, text: "Contact",link:"/contact" },
   ];
   const [isOpen, setIsOpen] = useState(false);
   return (
@@ -15,34 +16,37 @@ const Navbar = () => {
       <header className="bg-primary sticky top-0 z-[3000]">
         <nav className="flex justify-between items-center  w-[92%] mx-auto py-2 ">
           <div>
-            <a href="./index.html">
-              <p className="text-3xl text-white">AESA</p>
-            </a>
+            <NavLink to="/">
+              <p className="text-3xl text-white font-secondaryFont">AESA</p>
+            </NavLink>
           </div>
           <div
             className={`nav-links md:static absolute text-white md:min-h-fit md:w-auto  left-0 w-full flex items-center md:px-5 px-0 transition-all ease-in duration-500 top-14`}
             style={{ top: isOpen ? "50px" : "-400px" }}
           >
-            <ul className="flex md:flex-row px-3 py-4 w-full flex-col md:bg-primary bg-primary/95 backdrop-blur-sm md:items-center md:gap-[4vw] gap-8">
+            <ul className="flex md:flex-row px-3 py-4 w-full flex-col md:bg-primary bg-primary/95 backdrop-blur-sm md:items-center md:gap-[4vw] gap-8 font-mainFont">
               {navItems.map((navItem) => (
+                <NavLink to={navItem.link} className = {(nav)=>(nav.isActive?"active":"")}>
                 <li>
-                  <a
-                    className="hover:text-secondary hover:border-b-2 hover:border-white hover:pb-2"
+                  <span
+                    className="hover:text-secondary  cursor-pointer"
                     href="/"
                   >
                     {navItem.text}
-                  </a>
+                  </span>
                 </li>
+                </NavLink>
               ))}
             </ul>
           </div>
           <div className="flex items-center gap-6">
-            <a
-              href="./login.html"
+            <NavLink to="./login">
+            <span
               className="bg-white text-primary px-5 py-1 rounded-lg border-2 border-white hover:bg-primary hover:text-white transition duration-700 ease-in-out"
             >
               Se connecter
-            </a>
+            </span>
+            </NavLink>
             <span className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
               {isOpen ? (
                 <FontAwesomeIcon
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-// import './App.css'
+ import './index.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
-import Navbar from '../components/navbar';
+// Pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Activity from './pages/Activity'
+import Contact from './pages/Contact'
+import Event from './pages/Event'
+import Login from './pages/Login'
 // bibliothèque de routage
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -31,7 +37,14 @@ function App() {
     <>
      <BrowserRouter>
      <Routes>
-      <Route path='/' element=''/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/activity' element={<Activity/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/event' element={<Event/>}/>
+      <Route path='/login' element={<Login/>}/>
+      {/* Pour n'importe quel liens qui n'est pas définie au dessus */}
+      <Route path='*' element={<Home/>}/>
      </Routes>
      </BrowserRouter>
     </>
